0ce7ad4f9c992925e1e3644487df8bc040fbb227
opto_1 <opto_1@DESKTOP-3INBLBR>
2023-07-26 15:51:15 +0900

    csv파일 저장 경로 기능 추가

--- Services/CsvService.cs
+++ Services/CsvService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualBasic;
+using Microsoft.Win32;
 using System;
 using System.IO;
 using System.Text;
@@ -13,25 +14,52 @@ namespace MonitoringSensor.Services
         private StreamWriter writer; // CSV 파일 작성자
 
 
+        //public bool CreateCsv(string line)
+        //{
+        //    string defaultCsvName = DateTime.Now.ToString("yyMMdd_HHmm");
+        //    csvName = Interaction.InputBox("저장할 csv파일의 이름을 입력하세요:", "사용할 csv파일 이름 입력", defaultCsvName);
+        //    if (csvName == "")
+        //    {
+        //        return false;
+        //    }
+        //    else
+        //    {
+        //        string projectPath = Directory.GetCurrentDirectory();
+        //        csvFilePath = System.IO.Path.Combine(projectPath, csvName + ".csv");
+        //        writer = new StreamWriter(csvFilePath, true, Encoding.UTF8);
+        //        writer.WriteLine(line);
+        //        writer.Close();
+        //        MessageBox.Show(csvName + ".csv 저장 완료");
+        //        return true;
+
+        //    }
+        //}
+
         public bool CreateCsv(string line)
         {
-            string defaultCsvName = DateTime.Now.ToString("yyMMdd_HHmm");
-            csvName = Interaction.InputBox("저장할 csv파일의 이름을 입력하세요:", "사용할 csv파일 이름 입력", defaultCsvName);
-            if (csvName == "")
+            string currentDate = DateTime.Now.ToString("yyMMdd_HHmm");
+
+            var dialog = new SaveFileDialog
             {
-                return false;
-            }
-            else
+                FileName = currentDate,
+                Filter = "CSV Files (*.csv)|*.csv",
+                DefaultExt = "csv",
+                AddExtension = true
+            };
+
+            if (dialog.ShowDialog() == true)
             {
-                string projectPath = Directory.GetCurrentDirectory();
-                csvFilePath = System.IO.Path.Combine(projectPath, csvName + ".csv");
+                // 선택한 경로로 CSV 파일을 저장합니다.
+                csvFilePath = dialog.FileName;
+
                 writer = new StreamWriter(csvFilePath, true, Encoding.UTF8);
                 writer.WriteLine(line);
                 writer.Close();
-                MessageBox.Show(csvName + ".csv 저장 완료");
+                MessageBox.Show("CSV file saved successfully.");
                 return true;
-
             }
+
+            return false;
         }
 
         public void AddCsv(string timer, string data)
